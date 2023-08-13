0dc6a2837a8cbdf9fabec6d40e22bd13c23e4aa8
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-13 15:59:17 +0330

    add project colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
+    colors: {
+      primary: {
+        1: "#3B71FE",
+        2: "#8BC5E5",
+        3: "#92A5EF",
+        4: "#58C27D"
+      },
+      secondary: {
+        1: "#A4CDE3",
+        2: "#E4D7CF",
+        3: "#FFD166",
+        4: "#FA8F54"
+      },
+      neutral: {
+        1: "#141416",
+        2: "#23262F",
+        3: "#353945",
+        4: "#777E90",
+        5: "#B1B5C3",
+        6: "#E6E8EC",
+        7: "#F4F5F6",
+        8: "#FCFCFD"
+      }
+    },
   },
   plugins: []
 };
