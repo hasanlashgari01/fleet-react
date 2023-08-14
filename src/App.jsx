24eba7e9a11bb763cf940a55e992f67c096c66de
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-14 23:08:12 +0330

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { useRoutes } from "react-router";
 import Header from "./components/Header.jsx";
+import routes from "./routes.jsx";
 
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+    const router = useRoutes(routes);
+
+    return (
+        <>
+            <Header />
+            {router}
+        </>
+    );
 }
 
 export default App;
---
+++ src/routes.jsx
@@ -0,0 +1,5 @@
+import Home from "./pages/main/Home";
+
+const routes = [{ path: "/", element: <Home /> }];
+
+export default routes;
