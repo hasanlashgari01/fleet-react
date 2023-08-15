547395cf449d5176fabf71790da64c479c5264c7
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-15 15:49:42 +0330

    search box style done

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -17,12 +17,12 @@ function SearchBox() {
 
         const line = searchRef.current.nextElementSibling.firstElementChild;
         line.style.width = `${activeLinkWidth}px`;
-        line.style.left = `${activeLinkPosition - 20}px`;
+        line.style.left = `${activeLinkPosition}px`;
     };
 
     return (
-        <div className=" absolute inset-x-1 bottom-1 h-44 p-5 bg-gradient-to-tr from-neutral-8 from-40% to-neutral-8/[83%] to-100% border border-neutral-8 backdrop-blur-lg shadow-dropdown rounded-[20px] z-50">
-            <div>
+        <div className="absolute inset-x-1 lg:inset-x-10 2xl:inset-x-20 top-[80%] p-5 lg:p-10 bg-gradient-to-br from-neutral-8 from-0% to-neutral-8/[83%] to-100% border border-neutral-8 backdrop-blur-lg shadow-dropdown rounded-[20px] z-50">
+            <div className="mb-6">
                 <nav className="flex gap-x-8 w-full" ref={searchRef}>
                     <span to="/" className="search__link search__link--active" onClick={(e) => linkHandler(e)}>
                         Stays
@@ -39,6 +39,93 @@ function SearchBox() {
                 </nav>
                 <div className="relative mt-[30px] w-full h-[1px] bg-neutral-6 rounded-[1px]">
                     <span className="absolute left-0 inline-block w-[38px] h-full bg-neutral-2 rounded-sm transition-all duration-500 ease-linear"></span>
+                </div>
+            </div>
+            <div className="relative">
+                <div className="grid grid-cols-1 xs:grid-cols-2 2xl:grid-cols-4 h-fit">
+                    <div className="flex items-start gap-x-4 p-2 lg:p-4 order-first">
+                        <svg className="w-6 h-6 shrink-0 text-neutral-5">
+                            <use href="#location-arrow"></use>
+                        </svg>
+                        <div className="flex flex-col">
+                            <input
+                                id="location"
+                                type="text"
+                                placeholder="Location"
+                                className="w-full bg-transparent text-neutral-2 placeholder:text-neutral-2 font-PoppinsSemiBold text-lg lg:text-2xl outline-0"
+                            />
+                            <label
+                                htmlFor="location"
+                                className="w-full text-neutral-4 font-PoppinsRegular text-xs lg:text-base"
+                            >
+                                Where are you going?
+                            </label>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-1 xs:grid-cols-2 col-span-2">
+                        <div className="flex items-start gap-x-4 p-2 lg:p-4">
+                            <svg className="w-6 h-6 shrink-0 text-neutral-5">
+                                <use href="#calendar"></use>
+                            </svg>
+                            <div className="flex flex-col">
+                                <input
+                                    id="date"
+                                    type="text"
+                                    placeholder="Check in"
+                                    className="w-full bg-transparent text-neutral-2 placeholder:text-neutral-2 font-PoppinsSemiBold text-lg lg:text-2xl outline-0"
+                                />
+                                <label
+                                    htmlFor="location"
+                                    className="text-neutral-4 font-PoppinsRegular text-xs lg:text-base"
+                                >
+                                    Add date
+                                </label>
+                            </div>
+                        </div>
+                        <div className="flex items-start gap-x-4 p-2 lg:p-4">
+                            <svg className="w-6 h-6 shrink-0 text-neutral-5">
+                                <use href="#calendar"></use>
+                            </svg>
+                            <div className="flex flex-col">
+                                <input
+                                    id="location"
+                                    type="text"
+                                    placeholder="Check out"
+                                    className="w-full bg-transparent text-neutral-2 placeholder:text-neutral-2 font-PoppinsSemiBold text-lg lg:text-2xl outline-0"
+                                />
+                                <label
+                                    htmlFor="location"
+                                    className="text-neutral-4 font-PoppinsRegular text-xs lg:text-base"
+                                >
+                                    Add date
+                                </label>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex items-start gap-x-4 p-2 lg:p-4 xs:order-first 2xl:order-last">
+                        <svg className="w-6 h-6 shrink-0 text-neutral-5">
+                            <use href="#user"></use>
+                        </svg>
+                        <div className="flex flex-col">
+                            <input
+                                id="travelers"
+                                type="text"
+                                placeholder="Travelers"
+                                className="w-full bg-transparent text-neutral-2 placeholder:text-neutral-2 font-PoppinsSemiBold text-lg lg:text-2xl outline-0"
+                            />
+                            <label
+                                htmlFor="travelers"
+                                className="text-neutral-4 font-PoppinsRegular text-xs lg:text-base"
+                            >
+                                Add guests
+                            </label>
+                        </div>
+                    </div>
+                </div>
+                <div className="absolute bottom-0 right-0 bg-primary-1 p-4 rounded-full lg:cursor-pointer">
+                    <svg className="w-6 h-6 text-white">
+                        <use href="#search"></use>
+                    </svg>
                 </div>
             </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -19,32 +19,32 @@ export default {
                 175: "175%",
                 248: "248%",
             },
-        },
-        colors: {
-            primary: {
-                1: "#3B71FE",
-                2: "#8BC5E5",
-                3: "#92A5EF",
-                4: "#58C27D",
-            },
-            secondary: {
-                1: "#A4CDE3",
-                2: "#E4D7CF",
-                3: "#FFD166",
-                4: "#FA8F54",
-            },
-            neutral: {
-                1: "#141416",
-                2: "#23262F",
-                3: "#353945",
-                4: "#777E90",
-                5: "#B1B5C3",
-                6: "#E6E8EC",
-                7: "#F4F5F6",
-                8: "#FCFCFD",
-            },
-            gradient: {
-                1: "#ecf8fc",
+            colors: {
+                primary: {
+                    1: "#3B71FE",
+                    2: "#8BC5E5",
+                    3: "#92A5EF",
+                    4: "#58C27D",
+                },
+                secondary: {
+                    1: "#A4CDE3",
+                    2: "#E4D7CF",
+                    3: "#FFD166",
+                    4: "#FA8F54",
+                },
+                neutral: {
+                    1: "#141416",
+                    2: "#23262F",
+                    3: "#353945",
+                    4: "#777E90",
+                    5: "#B1B5C3",
+                    6: "#E6E8EC",
+                    7: "#F4F5F6",
+                    8: "#FCFCFD",
+                },
+                gradient: {
+                    1: "#ecf8fc",
+                },
             },
         },
         fontFamily: {
