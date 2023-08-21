c177fb7d739c0cbc562831d035c7b35115c2f97c
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-21 11:56:57 +0330

    product host

--- src/pages/home/Host.jsx
+++ src/pages/home/Host.jsx
@@ -11,6 +11,29 @@ function Host() {
                     <span className="py-2.5 px-3.5">Year</span>
                 </FilterDropdown>
             </div>
+            <div className="my-10">
+                <div className="relative bg-neutral-8 border border-neutral-6 rounded-3xl overflow-hidden">
+                    <span className="absolute top-5 left-4 flex items-center gap-x-1 py-0.5 px-2 bg-neutral-8 rounded-3xl">
+                        <svg className="w-3 h-3">
+                            <use href="#star"></use>
+                        </svg>
+                        <span className="text-neutral-2 font-PoppinsSemiBold text-xs/5">4.9</span>
+                    </span>
+                    <img
+                        src="/src/assets/images/hosts/host-1.jpg"
+                        alt=""
+                        className="w-full aspect-square object-cover"
+                    />
+                    <div className="relative pt-14 pb-8 px-10 text-center">
+                        <div className="absolute -top-1/2 left-1/2 -translate-x-1/2 translate-y-1/3 w-20 h-20 border-4 border-neutral-8 rounded-full overflow-hidden bg-red-500">
+                            <img src="/src/assets/images/Profile-1.jpg" alt="" className="w-full h-full aspect-square object-cover" />
+                        </div>
+                        <h3 className="text-neutral-2 font-PoppinsMedium text-base">Antone Heller</h3>
+                        <span className="mt-1 text-neutral-4 text-xs/5">Gaylordside</span>
+                    </div>
+                </div>
+                <div></div>
+            </div>
         </div>
     );
 }
