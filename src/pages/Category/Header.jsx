34e89b38dcf582c7d256fd8f425f48eb4f982c0d
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-08-25 16:38:59 +0330

    dark mode handle

--- src/pages/Category/Header.jsx
+++ src/pages/Category/Header.jsx
@@ -8,29 +8,32 @@ function Header() {
             <div className="flex justify-between">
                 <div className="flex gap-x-3">
                     <Link to="/">
-                        <svg className="inline-block lg:hidden w-10 h-10 text-neutral-2">
+                        <svg className="inline-block lg:hidden w-10 h-10 text-neutral-2 dark:text-neutral-8">
                             <use href="#arrow-left-simple"></use>
                         </svg>
                     </Link>
-
                     <div>
-                        <h1 className="text-neutral-2 font-DMSansBold text-[32px]/10">Places to stay</h1>
+                        <h1 className="text-neutral-2 dark:text-neutral-8 font-DMSansBold text-[32px]/10">
+                            Places to stay
+                        </h1>
                         <div className="mt-3 space-x-4">
-                            <span className="p-2 text-primary-4 font-PoppinsBold text-xs/3 uppercase border-2 border-primary-4 rounded">
+                            <span className="py-1.5 px-2 text-primary-4 font-PoppinsBold text-xs/3 uppercase border-2 border-primary-4 rounded">
                                 300+ stays
                             </span>
-                            <span className="text-neutral-2 font-PoppinsRegular text-xs/5">May 1 - 14, 2 guests</span>
+                            <span className="text-neutral-2 dark:text-neutral-8 font-PoppinsRegular text-xs/5 lg:text-base/6">
+                                May 1 - 14, 2 guests
+                            </span>
                         </div>
                     </div>
                 </div>
-                <div className="hidden lg:inline-flex items-center gap-x-3 h-fit py-3 px-6 border-2 border-neutral-6 rounded-full">
-                    <span className="text-neutral-2 font-DMSansBold leading-4 cursor-pointer">Advanced filter</span>
+                <div className="hidden lg:inline-flex items-center gap-x-3 h-fit py-3 px-6 border-2 border-neutral-6 dark:border-neutral-3 rounded-full">
+                    <span className="text-neutral-2 dark:text-neutral-8 font-DMSansBold leading-4 cursor-pointer">Advanced filter</span>
                     <svg className="w-4 h-4 text-neutral-4">
                         <use href="#plus2"></use>
                     </svg>
                 </div>
             </div>
-            <span className="inline-block my-8 w-full h-0.5 bg-neutral-6"></span>
+            <span className="inline-block my-8 w-full h-[1px] bg-neutral-6 dark:bg-neutral-3"></span>
             <div className="flex flex-wrap gap-4 justify-between items-center">
                 <ul className="hidden lg:inline-block space-x-2">
                     <span className="inline-flex gap-x-2 py-1.5 px-3 bg-neutral-3 dark:bg-neutral-6 text-neutral-8 dark:text-neutral-2 font-DMSansBold text-sm/4 cursor-pointer rounded-full">
