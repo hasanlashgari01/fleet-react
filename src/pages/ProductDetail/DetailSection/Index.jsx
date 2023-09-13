a9910cda4b0953679b265437aa5c1aae02eedcfb
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-13 14:31:02 +0330

    add bg gradient

--- src/pages/ProductDetail/DetailSection/Index.jsx
+++ src/pages/ProductDetail/DetailSection/Index.jsx
@@ -3,7 +3,7 @@ import Aside from "./ReserveBox/Index";
 
 const ProductDetail = () => {
     return (
-        <div className="container py-16 lg:pb-20 2xl:pb-34 flex gap-x-12">
+        <div className="container py-16 lg:pb-20 2xl:pb-34 lg:flex gap-x-12">
             <div>
                 <LeftSection />
             </div>
--- src/pages/ProductDetail/DetailSection/ReserveBox/ReserveBoxMobile.jsx
+++ src/pages/ProductDetail/DetailSection/ReserveBox/ReserveBoxMobile.jsx
@@ -3,7 +3,7 @@ import ReserveInfo from "./Header/ReserveInfo";
 
 const ReserveBoxMobile = () => {
     return (
-        <div className="fixed bottom-0 left-0 right-0 flex justify-between lg:hidden gap-x-8 bg-white shadow-big h-[120px] p-8 z-30">
+        <div className="gradient-mobile fixed bottom-0 left-0 right-0 flex justify-between lg:hidden gap-x-8 shadow-big h-[120px] p-8 border border-neutral-6 rounded-t-3xl z-30">
             <ReserveInfo />
             <ReserveButton />
         </div>
