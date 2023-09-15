32f8ddaab47ef071dbf009c0152f543faad97a57
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-15 17:49:42 +0330

    dark mode property type

--- src/components/PropertyType/Card.jsx
+++ src/components/PropertyType/Card.jsx
@@ -4,7 +4,7 @@ const Card = () => {
     return (
         <>
             <div className="relative h-64 aspect-square rounded-3xl overflow-hidden">
-                <span className="absolute top-4 left-4 py-2 px-3 bg-neutral-2 text-neutral-8 font-PoppinsSemiBold text-xs uppercase rounded-full">
+                <span className="absolute top-4 left-4 py-2 px-3 bg-neutral-2 dark:bg-neutral-8 text-neutral-8 dark:text-neutral-2 font-PoppinsSemiBold text-xs uppercase rounded-full shadow-md">
                     20% off
                 </span>
                 <img
@@ -14,7 +14,7 @@ const Card = () => {
                 />
             </div>
             <div className="mt-3 lg:mt-[21px] px-3.5">
-                <h3 className="text-neutral-2 font-PoppinsMedium line-clamp-1">Nature house</h3>
+                <h3 className="text-neutral-2 dark:text-neutral-8 font-PoppinsMedium line-clamp-1">Nature house</h3>
                 <h4 className="flex items-center gap-x-1.5 mt-1.5 lg:mt-2 text-neutral-4">
                     <svg className="w-4 h-4">
                         <use href="#home"></use>
