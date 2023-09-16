5ed77e8b1878eba82d12bc59e2dfc045cfea8462
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-16 16:05:03 +0330

    change page scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import { useRoutes } from "react-router";
+import { useLocation, useRoutes } from "react-router";
 import Header from "./layouts/Header.jsx";
 import routes from "./routes/routes.jsx";
 import Footer from "./layouts/Footer.jsx";
+import { useEffect } from "react";
 
 function App() {
     const router = useRoutes(routes);
+    const location = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [location]);
 
     return (
         <>
