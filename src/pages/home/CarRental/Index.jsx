de09dddb91e9cc26fcf200ddb31029e67ece5c25
Hasan Lashgari <hasanlashgari01@gmail.com>
2023-09-24 19:42:58 +0330

    add breadcrumbs

--- src/pages/home/CarRental/Index.jsx
+++ src/pages/home/CarRental/Index.jsx
@@ -1,16 +1,20 @@
+import Breadcrumbs from "../../../components/home/Breadcrumbs";
 import Card from "./Card";
 
 const Index = () => {
     return (
-        <div className="container grid grid-cols-1 lg:grid-cols-2 gap-8">
-            <Card image="src/assets/images/cars/cars-1.jpg" />
-            <Card image="src/assets/images/cars/cars-2.jpg" />
-            <Card image="src/assets/images/cars/cars-1.jpg" />
-            <Card image="src/assets/images/cars/cars-2.jpg" />
-            <Card image="src/assets/images/cars/cars-1.jpg" />
-            <Card image="src/assets/images/cars/cars-2.jpg" />
-            <Card image="src/assets/images/cars/cars-1.jpg" />
-            <Card image="src/assets/images/cars/cars-2.jpg" />
+        <div className="container">
+            <Breadcrumbs subs={["Stays", "New Zealand", "South Island"]} />
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 mt-8 lg:mt-16 2xl:mt-20">
+                <Card image="src/assets/images/cars/cars-1.jpg" />
+                <Card image="src/assets/images/cars/cars-2.jpg" />
+                <Card image="src/assets/images/cars/cars-1.jpg" />
+                <Card image="src/assets/images/cars/cars-2.jpg" />
+                <Card image="src/assets/images/cars/cars-1.jpg" />
+                <Card image="src/assets/images/cars/cars-2.jpg" />
+                <Card image="src/assets/images/cars/cars-1.jpg" />
+                <Card image="src/assets/images/cars/cars-2.jpg" />
+            </div>
         </div>
     );
 };
